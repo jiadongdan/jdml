4d6775d51632a29c34eb22a89189bcf646c50952
Jiadong Dan <ted930511@gmail.com>
2026-02-05 16:14:24 +0800

    Update __init__.py

--- jdml/models/__init__.py
+++ jdml/models/__init__.py
@@ -1,4 +1,9 @@
 from .DnCNN import DnCNN
+from ._flexible_CNN_v4 import CNNModel, get_loss, get_optimizer, train_model
 
 __all__ = ['DnCNN',
+           'CNNModel',
+           'get_loss',
+           'get_optimizer',
+           'train_model',
            ]
