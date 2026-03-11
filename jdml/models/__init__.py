ea85b5c26857f5a387a2fe8b36e9af18c243b6f0
Jiadong Dan <ted930511@gmail.com>
2026-03-11 11:09:11 +0800

    add ViT model

--- jdml/models/__init__.py
+++ jdml/models/__init__.py
@@ -1,9 +1,11 @@
 from .DnCNN import DnCNN
 from ._flexible_CNN_v4 import CNNModel, get_loss, get_optimizer, train_model
+from .vit_jd import ViT
 
 __all__ = ['DnCNN',
            'CNNModel',
            'get_loss',
            'get_optimizer',
            'train_model',
+           'ViT',
            ]
